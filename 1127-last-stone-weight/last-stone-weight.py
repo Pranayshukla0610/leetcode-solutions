9bc0fb2e30a4208609878ae3909b25c346fa126b
Pranay Shukla <pranayshukla1020@gmail.com>
2026-03-22 02:23:16 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (95.71%) - LeetSync

---
+++ 1127-last-stone-weight/last-stone-weight.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        while len(stones) > 1:
+            stones.sort()  # sort every time
+            
+            x = stones.pop()     # largest
+            y = stones.pop()     # second largest
+            
+            if x != y:
+                stones.append(x - y)
+        
+        return stones[0] if stones else 0
+        
